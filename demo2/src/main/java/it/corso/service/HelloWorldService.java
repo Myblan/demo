542bb717cff25cc25354cef41ee4f567df0a58c4
MYBLAN SIMEN TCHALLE <msimentc@emeal.nttdata.com>
2024-09-14 10:46:20 +0200

    aggiunta service

---
+++ demo2/src/main/java/it/corso/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package it.corso.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String getService(){
+        return "Hello World Service";
+    }
+}
