3382ed1f96eda41c72bb8a251d68e5bd044fa8e3
MYBLAN SIMEN TCHALLE <msimentc@emeal.nttdata.com>
2024-09-14 10:59:46 +0200

    aggiunta model

---
+++ demo2/src/main/java/it/corso/model/HelloWorldModel.java
@@ -0,0 +1,14 @@
+package it.corso.model;
+
+public class HelloWorldModel {
+
+    private String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
